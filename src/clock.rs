6a2664e0c5abbd647c9063db7b3924fc81babec5
Dave Hylands <davehylands@fullstory.com>
2020-05-15 20:00:21 -0700

    Finally have a version of clock that allows the main app to specoify the constants

--- src/clock.rs
+++ src/clock.rs
@@ -3,36 +3,56 @@
 use drone_cortexm::{fib, fib::ThrFiberFuture, reg::prelude::*, thr::ThrNvic};
 use drone_stm32_map::reg;
 
+pub const SYSCLK_SW_HSI: u32 = 0b00;
+pub const SYSCLK_SW_HSE: u32 = 0b01;
+pub const SYSCLK_SW_PLL: u32 = 0b10;
+
+pub const APB_DIV1: u32 = 0b000;
+pub const APB_DIV2: u32 = 0b100;
+pub const APB_DIV4: u32 = 0b101;
+pub const APB_DIV8: u32 = 0b110;
+pub const APB_DIV16: u32 = 0b111;
+
+pub const AHB_DIV1: u32 = 0b0000;
+pub const AHB_DIV2: u32 = 0b1000;
+pub const AHB_DIV3: u32 = 0b1001;
+pub const AHB_DIV8: u32 = 0b1010;
+pub const AHB_DIV16: u32 = 0b1011;
+pub const AHB_DIV64: u32 = 0b1100;
+pub const AHB_DIV128: u32 = 0b1101;
+pub const AHB_DIV256: u32 = 0b1110;
+pub const AHB_DIV512: u32 = 0b1111;
+
 pub const PLLP_DIV2: u32 = 0b00;
 
-pub trait SystemClockConsts {
+pub trait SystemClock {
     /// Specifies the number of wait states to set for FLASH_ACR register.
-    const FLASH_LATENCY: u32;
+    fn flash_latency(&self) -> u32;
 
     /// HSE crystal frequency.
-    const HSE_FREQ: u32;
+    fn hse_freq(&self) -> u32;
 
     /// HSI crystal frequency.
-    const HSI_FREQ: u32;
+    fn hsi_freq(&self) -> u32;
 
     // VCO-freq = HSE * (PLLN / PLLM)
     // PLL general clock = VCO-freq / PLLP
     // USB/SDIO/RG freq = VCO-freq / PLLQ
 
     /// PLLM - Division factor for the main PLL (PLL) input clock
-    const PLLM: u32;
+    fn pllm(&self) -> u32;
 
     /// PLLN - Main PLL multiplication factor for VCO
-    const PLLN: u32;
+    fn plln(&self) -> u32;
 
     /// PLLP - Main PLL division factor for main system clock
-    const PLLP: u32;
+    fn pllp(&self) -> u32;
 
     /// PLLQ - Main PLL division factor for USB OTG FS, and SDIO clocks
-    const PLLQ: u32;
+    fn pllq(&self) -> u32;
 }
 
-pub struct SystemClock<THR: ThrNvic + ThrFiberFuture> {
+pub struct SystemClockRegs<'a, THR: ThrNvic + ThrFiberFuture> {
     flash_acr: reg::flash::Acr<Srt>,
     pwr_cr: reg::pwr::Cr<Srt>,
     rcc_cr: reg::rcc::Cr<Srt>,
@@ -41,9 +61,10 @@ pub struct SystemClock<THR: ThrNvic + ThrFiberFuture> {
     rcc_cir: reg::rcc::Cir<Crt>,
     rcc_apb1enr: reg::rcc::Apb1Enr<Srt>,
     thr_rcc: THR,
+    sysclk: &'a dyn SystemClock,
 }
 
-impl<THR: ThrNvic + ThrFiberFuture> SystemClock<THR> {
+impl <'a, THR: ThrNvic + ThrFiberFuture> SystemClockRegs<'a, THR> {
     pub fn init(
         flash_acr: reg::flash::Acr<Srt>,
         pwr_cr: reg::pwr::Cr<Srt>,
@@ -53,6 +74,7 @@ impl<THR: ThrNvic + ThrFiberFuture> SystemClock<THR> {
         rcc_cir: reg::rcc::Cir<Crt>,
         rcc_apb1enr: reg::rcc::Apb1Enr<Srt>,
         thr_rcc: THR,
+        sysclk: &'a dyn SystemClock,
     ) -> Self {
         Self {
             flash_acr,
@@ -63,27 +85,25 @@ impl<THR: ThrNvic + ThrFiberFuture> SystemClock<THR> {
             rcc_cir,
             rcc_apb1enr,
             thr_rcc,
+            sysclk,
         }
     }
 
+    /// Returns the current frequency of the system clock.
     pub fn clock(&self) -> u32
-    where
-        Self: SystemClockConsts,
     {
-        ((Self::HSE_FREQ / self.rcc_pllcfgr.pllm.read_bits()) * self.rcc_pllcfgr.plln.read_bits())
+        ((self.sysclk.hse_freq() / self.rcc_pllcfgr.pllm.read_bits()) * self.rcc_pllcfgr.plln.read_bits())
             / ((self.rcc_pllcfgr.pllp.read_bits() + 1) * 2)
     }
 
+    /// Returns the current frequency of the SysTick clock.
     pub fn systick_frequency(&self) -> u32
-    where
-        Self: SystemClockConsts,
     {
         self.clock() / 8
     }
 
+    /// Sets the system clock frequency based on the constants.
     pub async fn raise_system_frequency(&self)
-    where
-        Self: SystemClockConsts,
     {
         // Enable Power Control Clock.
         self.rcc_apb1enr.pwren.set_bit();
@@ -129,10 +149,10 @@ impl<THR: ThrNvic + ThrFiberFuture> SystemClock<THR> {
 
         self.rcc_pllcfgr.modify(|r| {
             r.set_pllsrc() // HSE oscillator clock selected as PLL input clock
-                .write_pllm(Self::PLLM)
-                .write_plln(Self::PLLN)
-                .write_pllp(Self::PLLP)
-                .write_pllq(Self::PLLQ)
+                .write_pllm(self.sysclk.pllm())
+                .write_plln(self.sysclk.plln())
+                .write_pllp(self.sysclk.pllp())
+                .write_pllq(self.sysclk.pllq())
         });
         // Enable the PLL.
         self.rcc_cr.modify(|r| r.set_pllon());
@@ -141,20 +161,20 @@ impl<THR: ThrNvic + ThrFiberFuture> SystemClock<THR> {
 
         // The power-on reset latency is set to zero. Since we're going to be increasing
         // the clock speed, increase the latency before increasing the clock speed.
-        self.flash_acr
-            .modify(|r| r.write_latency(Self::FLASH_LATENCY));
-        if self.flash_acr.load().latency() != Self::FLASH_LATENCY {
+        let flash_latency = self.sysclk.flash_latency();
+        self.flash_acr.modify(|r| r.write_latency(flash_latency));
+        if self.flash_acr.load().latency() != flash_latency {
             panic!("LATENCY");
         }
 
         self.rcc_cfgr.modify(|r| {
-            r.write_ppre1(0b101) // 0b101 = Divide by 4 - APB1 - 24 MHz (must be < 45 MHz)
-                .write_ppre2(0b100) // 0b100 = Divide by 2 - APB2 - 48 MHz (must be < 90 MHz)
-                .write_hpre(0b0000) // 0b0000 = Divide by 1 - AHB - 96 MHz
-                .write_sw(0b10) // 0b10 = Use PLL for System Clock - 96 MHz
+            r.write_ppre1(APB_DIV4) //    APB1 = AHB / 4 = 24 MHz (must be < 45 MHz)
+                .write_ppre2(APB_DIV2) // APB2 = AHB / 2 = 48 MHz (must be < 90 MHz)
+                .write_hpre(AHB_DIV1) //  AHB = SYSCLK / 1 = 96 MHz
+                .write_sw(SYSCLK_SW_PLL) // Use PLL for System Clock - 96 MHz
         });
 
-        if self.rcc_cfgr.load().sws() != 0b10 {
+        if self.rcc_cfgr.load().sws() != SYSCLK_SW_PLL {
             panic!("SYSCLK not set to HSE");
         }
     }
